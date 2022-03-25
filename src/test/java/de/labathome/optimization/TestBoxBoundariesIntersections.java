4af4722150108d370e90cd663e63bee510e486ff
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-03-25 19:25:42 +0100

    box-line-intersections tests done

--- src/test/java/de/labathome/optimization/TestBoxBoundariesIntersections.java
+++ src/test/java/de/labathome/optimization/TestBoxBoundariesIntersections.java
@@ -70,15 +70,15 @@ public class TestBoxBoundariesIntersections {
 		IntersectionResult r6b = QuadraticProgrammingSubproblem.boxIntersections(
 				new double[] {2.0, 0.0},
 				new double[] {0.0, 2.0},
-				new double[] {-3.0,  3.0},
-				new double[] {-1.0,  1.0});
+				new double[] {-3.0, 3.0},
+				new double[] {-1.0, 1.0});
 		Assertions.assertFalse(r6b.intersect());
 
 		IntersectionResult r6c = QuadraticProgrammingSubproblem.boxIntersections(
 				new double[] {2.0, 0.0},
 				new double[] {0.0, 2.0},
-				new double[] {-3.0,  Double.NEGATIVE_INFINITY},
-				new double[] {-1.0,  Double.POSITIVE_INFINITY});
+				new double[] {-3.0, Double.NEGATIVE_INFINITY},
+				new double[] {-1.0, Double.POSITIVE_INFINITY});
 		Assertions.assertFalse(r6c.intersect());
 
 		IntersectionResult r6d = QuadraticProgrammingSubproblem.boxIntersections(
@@ -106,4 +106,114 @@ public class TestBoxBoundariesIntersections {
 		Assertions.assertTrue(r7.intersect());
 	}
 
+	@Test
+	void test2dBoxConstraintsEntireLine() {
+		final double tolerance = 1.0e-15;
+
+		// Box constraint in the direction of vector d
+		IntersectionResult r1 = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {0.0, 2.0},
+				new double[] {1.0, 1.0},
+				new double[] {3.0, 3.0}, true);
+		MinervaAssertions.assertRelAbsEquals(0.5, r1.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals(1.5, r1.tB(), tolerance);
+		Assertions.assertTrue(r1.intersect());
+
+		// Negative direction
+		IntersectionResult r2 = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {0.0, 2.0},
+				new double[] {1.0, -3.0},
+				new double[] {3.0, -1.0}, true);
+		MinervaAssertions.assertRelAbsEquals(-1.5, r2.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals(-0.5, r2.tB(), tolerance);
+		Assertions.assertTrue(r2.intersect());
+
+		// Some constraints are absent (set to +/- inf)
+		IntersectionResult r3 = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {0.0, 2.0},
+				new double[] {Double.NEGATIVE_INFINITY, 1.0},
+				new double[] {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY}, true);
+		MinervaAssertions.assertRelAbsEquals(0.5, r3.tA(), tolerance);
+		Assertions.assertEquals(Double.POSITIVE_INFINITY, r3.tB());
+		Assertions.assertTrue(r3.intersect());
+
+		// Intersect on the face of the box
+		IntersectionResult r4 = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {1.0, 0.0},
+				new double[] {0.0, 1.0},
+				new double[] {1.0, 1.0},
+				new double[] {3.0, 3.0}, true);
+		MinervaAssertions.assertRelAbsEquals(1.0, r4.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals(3.0, r4.tB(), tolerance);
+		Assertions.assertTrue(r4.intersect());
+
+		// Interior initial pointoint
+		IntersectionResult r5 = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {0.0, 0.0},
+				new double[] {4.0, 4.0},
+				new double[] {-2.0, -3.0},
+				new double[] { 3.0,  2.0}, true);
+		MinervaAssertions.assertRelAbsEquals(-0.5, r5.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals( 0.5, r5.tB(), tolerance);
+		Assertions.assertTrue(r5.intersect());
+
+        // No intersection between line and box constraints
+		IntersectionResult r6a = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {0.0, 2.0},
+				new double[] {-3.0, -3.0},
+				new double[] {-1.0, -1.0}, true);
+		Assertions.assertFalse(r6a.intersect());
+
+		IntersectionResult r6b = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {0.0, 2.0},
+				new double[] {-3.0, 3.0},
+				new double[] {-1.0, 1.0});
+		Assertions.assertFalse(r6b.intersect());
+
+		IntersectionResult r6c = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {0.0, 2.0},
+				new double[] {-3.0, Double.NEGATIVE_INFINITY},
+				new double[] {-1.0, Double.POSITIVE_INFINITY}, true);
+		Assertions.assertFalse(r6c.intersect());
+
+		IntersectionResult r6d = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {0.0, 0.0},
+				new double[] {1.0, 100.0},
+				new double[] {1.0, 1.0},
+				new double[] {3.0, 3.0}, true);
+		Assertions.assertFalse(r6d.intersect());
+
+		IntersectionResult r6e = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {0.99, 0.0},
+				new double[] {0.0,  2.0},
+				new double[] {1.0,  1.0},
+				new double[] {3.0,  3.0});
+		Assertions.assertFalse(r6e.intersect());
+
+		// Initial point on the boundary
+		IntersectionResult r7 = QuadraticProgrammingSubproblem.boxIntersections(
+				new double[] {2.0, 2.0},
+				new double[] {0.0, 1.0},
+				new double[] {-2.0, -2.0},
+				new double[] { 2.0,  2.0}, true);
+		MinervaAssertions.assertRelAbsEquals(-4.0, r7.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals( 0.0, r7.tB(), tolerance);
+		Assertions.assertTrue(r7.intersect());
+	}
+
+	void test3dBoxConstraints() {
+
+
+	}
+
+	void test3dBoxConstraintsEntireLine() {
+
+
+	}
 }

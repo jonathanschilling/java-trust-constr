d6811f0b405d0a8989e43ed41a30c50f94b31c82
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-03-25 18:07:53 +0100

    all tests for sphereIntersections

--- src/test/java/de/labathome/optimization/TestSphericalBoundariesIntersections.java
+++ src/test/java/de/labathome/optimization/TestSphericalBoundariesIntersections.java
@@ -9,17 +9,88 @@ class TestSphericalBoundariesIntersections {
 
 	@Test
 	void test2dSphereConstraints() {
+		final double tolerance = 1.0e-15;
 
 		// Interior inicial point
 		IntersectionResult r1 = EQPProblem.sphereIntersections(
 				new double[] {0.0, 0.0},
 				new double[] {1.0, 0.0}, 0.5);
 
-		final double tolerance = 1.0e-15;
 		MinervaAssertions.assertRelAbsEquals(0.0, r1.tA(), tolerance);
 		MinervaAssertions.assertRelAbsEquals(0.5, r1.tB(), tolerance);
 		Assertions.assertTrue(r1.intersect());
 
+		// No intersection between line and circle
+		IntersectionResult r2 = EQPProblem.sphereIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {0.0, 1.0}, 1.0);
+		Assertions.assertFalse(r2.intersect());
 
+		// Outside initial point pointing toward outside the circle
+		IntersectionResult r3 = EQPProblem.sphereIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {1.0, 0.0}, 1.0);
+		Assertions.assertFalse(r3.intersect());
+
+		// Outside initial point pointing toward inside the circle
+		IntersectionResult r4 = EQPProblem.sphereIntersections(
+				new double[] { 2.0, 0.0},
+				new double[] {-1.0, 0.0}, 1.5);
+
+		MinervaAssertions.assertRelAbsEquals(0.5, r4.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals(1.0, r4.tB(), tolerance);
+		Assertions.assertTrue(r4.intersect());
+
+		// Initial point on the boundary
+		IntersectionResult r5 = EQPProblem.sphereIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {1.0, 0.0}, 2.0);
+
+		MinervaAssertions.assertRelAbsEquals(0.0, r5.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals(0.0, r5.tB(), tolerance);
+		Assertions.assertTrue(r5.intersect());
+	}
+
+	@Test
+	void test2dSphereConstraintsLineIntersections() {
+		final double tolerance = 1.0e-15;
+
+		// Interior initial point
+		IntersectionResult r1 = EQPProblem.sphereIntersections(
+				new double[] {0.0, 0.0},
+				new double[] {1.0, 0.0}, 0.5, true);
+		MinervaAssertions.assertRelAbsEquals(-0.5, r1.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals( 0.5, r1.tB(), tolerance);
+		Assertions.assertTrue(r1.intersect());
+
+		// No intersection between line and circle
+		IntersectionResult r2 = EQPProblem.sphereIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {0.0, 1.0}, 1.0, false);
+		Assertions.assertFalse(r2.intersect());
+
+		// Outside initial point pointing toward outside the circle
+		IntersectionResult r3 = EQPProblem.sphereIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {1.0, 0.0}, 1.0, true);
+		MinervaAssertions.assertRelAbsEquals(-3.0, r3.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals(-1.0, r3.tB(), tolerance);
+		Assertions.assertTrue(r3.intersect());
+
+		// Outside initial point pointing toward inside the circle
+		IntersectionResult r4 = EQPProblem.sphereIntersections(
+				new double[] { 2.0, 0.0},
+				new double[] {-1.0, 0.0}, 1.5, true);
+		MinervaAssertions.assertRelAbsEquals(0.5, r4.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals(3.5, r4.tB(), tolerance);
+		Assertions.assertTrue(r4.intersect());
+
+		// Initial point on the boundary
+		IntersectionResult r5 = EQPProblem.sphereIntersections(
+				new double[] {2.0, 0.0},
+				new double[] {1.0, 0.0}, 2.0, true);
+		MinervaAssertions.assertRelAbsEquals(-4.0, r5.tA(), tolerance);
+		MinervaAssertions.assertRelAbsEquals( 0.0, r5.tB(), tolerance);
+		Assertions.assertTrue(r5.intersect());
 	}
 }

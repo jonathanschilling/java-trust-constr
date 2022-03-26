0ce658363bb4f51125c318663125e4da1f582517
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-03-26 20:55:25 +0100

    testCauchyPointEqualToNewtonPoint() completed; prototype for PCG tests

--- src/test/java/de/labathome/optimization/TestModifiedDogleg.java
+++ src/test/java/de/labathome/optimization/TestModifiedDogleg.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.ujmp.core.Matrix;
 import org.ujmp.core.doublematrix.DoubleMatrix2D;
 
+import de.labathome.LinAlg;
 import de.labathome.LinearOperator;
 import minerva.tests.junit.MinervaAssertions;
 
@@ -19,19 +20,32 @@ class TestModifiedDogleg {
 		LinearOperator[] projections = Projections.projections(A);
 		LinearOperator Y = projections[2];
 
-		double[] newtonPoint = { 0.24615385, 1.96923077 };
+		Matrix newtonPoint = Matrix.Factory.linkToArray(new double[] { 0.24615385, 1.96923077 });
 
 		// Newton point inside boundaries
 		Matrix x1 = QuadraticProgrammingSubproblem.modifiedDogleg(A, Y, b, 2.0,
 				new double[] {Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY},
 				new double[] {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY});
+		MinervaAssertions.assertArrayRelAbsEquals(LinAlg.col(newtonPoint), LinAlg.col(x1), tolerance);
 
-		double[] x1Arr = x1.transpose().toDoubleArray()[0];
-		MinervaAssertions.assertArrayRelAbsEquals(newtonPoint, x1Arr, tolerance);
+		// Spherical constraint active
+		Matrix x2 = QuadraticProgrammingSubproblem.modifiedDogleg(A, Y, b, 1.0,
+				new double[] {Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY},
+				new double[] {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY});
+		Matrix normNewtonPt = newtonPoint.divide(newtonPoint.norm2());
+		MinervaAssertions.assertArrayRelAbsEquals(LinAlg.col(normNewtonPt), LinAlg.col(x2), tolerance);
 
-		// TODO: Spherical constraint active
+		// Box constraints active
+		Matrix x3 = QuadraticProgrammingSubproblem.modifiedDogleg(A, Y, b, 2.0,
+				new double[] {Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY},
+				new double[] {0.1, Double.POSITIVE_INFINITY});
+		Matrix boxPoint = newtonPoint.times(0.1 / newtonPoint.getAsDouble(0, 0));
+		MinervaAssertions.assertArrayRelAbsEquals(LinAlg.col(boxPoint), LinAlg.col(x3), tolerance);
+	}
 
-		// TODO: Box constraints active
+	@Test
+	void test3dExample() {
+
 	}
 
 }
---
+++ src/test/java/de/labathome/optimization/TestProjectedCG.java
@@ -0,0 +1,67 @@
+package de.labathome.optimization;
+
+import org.junit.jupiter.api.Test;
+
+class TestProjectedCG {
+
+	@Test
+	void testNocedalExample() {
+
+	}
+
+	@Test
+	void testCompareWithDirectFact() {
+
+	}
+
+	@Test
+	void testTrustRegionInfeasible() {
+
+	}
+
+	@Test
+	void testTrustRegionBarelyFeasible() {
+
+	}
+
+	@Test
+	void testHitsBoundary() {
+
+	}
+
+	@Test
+	void testNegativeCurvatureUnconstrained() {
+
+	}
+
+	@Test
+	void testNegativeCurvature() {
+
+	}
+
+	@Test
+	void testInactiveBoxConstraints() {
+
+	}
+
+	@Test
+	void testActiveBoxConstraintsMaximumIterationsReached() {
+
+	}
+
+	@Test
+	void testActiveBoxConstraintsHitsBoundaries() {
+
+	}
+
+	@Test
+	void testActiveBoxConstraintsHitsBoundariesInfeasibleIter() {
+
+	}
+
+	@Test
+	void testActiveBoxConstraintsNegativeCurvature() {
+
+	}
+
+}

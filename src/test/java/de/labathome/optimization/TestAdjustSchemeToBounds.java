41ba9a7bdcd05a4ae465548088d8d49a0002c8f8
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-03-31 10:27:58 +0200

    testTightBounds works

--- src/test/java/de/labathome/optimization/TestAdjustSchemeToBounds.java
+++ src/test/java/de/labathome/optimization/TestAdjustSchemeToBounds.java
@@ -76,4 +76,31 @@ class TestAdjustSchemeToBounds {
 		MinervaAssertions.assertArrayRelAbsEquals(new double[] {0.1, -0.1, 0.1}, LinAlg.col(ads.hAdjusted()), tolerance);
 		Assertions.assertArrayEquals(new boolean[] {false, true, true}, ads.useOneSided());
 	}
+
+	@Test
+	void testTightBounds() {
+		final double tolerance = 1.0e-15;
+
+		Matrix x0 = Matrix.Factory.linkToArray(new double[] { 0.0,   0.03});
+		Matrix lb = Matrix.Factory.linkToArray(new double[] {-0.03, -0.03});
+		Matrix ub = Matrix.Factory.linkToArray(new double[] { 0.05,  0.05});
+		Matrix h = Matrix.Factory.linkToArray(new double[] {-0.1, -0.1});
+
+		// Make sure that one single-sided step fits between x0 and bounds.
+		AdjustedDifferencingScheme ads = NumDiff.adjustSchemeToBounds(x0, h, 1, FiniteDifferenceMethod.ONE_SIDED, lb, ub);
+		MinervaAssertions.assertArrayRelAbsEquals(new double[] {0.05, -0.06}, LinAlg.col(ads.hAdjusted()), tolerance);
+
+		// Make sure that two single-sided steps fit between x0 and bounds.
+		// --> steps have to be half the size than in above test case
+		ads = NumDiff.adjustSchemeToBounds(x0, h, 2, FiniteDifferenceMethod.ONE_SIDED, lb, ub);
+		MinervaAssertions.assertArrayRelAbsEquals(new double[] {0.025, -0.03}, LinAlg.col(ads.hAdjusted()), tolerance);
+
+		ads = NumDiff.adjustSchemeToBounds(x0, h, 1, FiniteDifferenceMethod.TWO_SIDED, lb, ub);
+		MinervaAssertions.assertArrayRelAbsEquals(new double[] {0.03, -0.03}, LinAlg.col(ads.hAdjusted()), tolerance);
+		Assertions.assertArrayEquals(new boolean[] {false, true}, ads.useOneSided());
+
+		ads = NumDiff.adjustSchemeToBounds(x0, h, 2, FiniteDifferenceMethod.TWO_SIDED, lb, ub);
+		MinervaAssertions.assertArrayRelAbsEquals(new double[] {0.015, -0.015}, LinAlg.col(ads.hAdjusted()), tolerance);
+		Assertions.assertArrayEquals(new boolean[] {false, true}, ads.useOneSided());
+	}
 }
